dc403594a12c40c997ce661520770c668ee8ee01
Aaron Shaver <AaronShaverPDX@gmail.com>
2018-10-10 11:46:51 -0700

    Tweaks, including a small performance one!

--- counter.py
+++ counter.py
@@ -16,32 +16,34 @@ def print_counts(counts):
             next_count = '{0} - {1}'.format(value, key)
             output_list.append(next_count)
             i += 1
+        else:
+            break  # no need to waste cycles if we've already done top 100/etc.
 
     final_output = ', '.join(output_list)
-    print(final_output)
+    print(final_output)  # TODO: thinking 10/4 should return str for pure func
 
 
 def sort_counts(counts):
     # sort by highest count first
-    return sorted(counts.items(), key=lambda key_value_pair: key_value_pair[1], reverse=True)
+    return sorted(counts.items(), key=lambda key_value_pair: key_value_pair[1],
+                  reverse=True)
 
 
 def count(text):
-    # 2% performance improvement; see notes.txt
-    local_separator = SEPARATOR
-    local_gram_size = GRAM_SIZE
-
     output = {}
 
     # pre-processing like lowercasing everything, removing non-alphanum chars
-    text = local_separator.join(text)
+    text = SEPARATOR.join(text)
     text = text.lower()
-    stripped_text = re.sub(r'[^a-z0-9 ]+', local_separator, text)
+    stripped_text = re.sub(r'[^a-z0-9 ]+', SEPARATOR, text)
     split_text = stripped_text.split()
 
-    if len(split_text) < local_gram_size:
+    if len(split_text) < GRAM_SIZE:  # avoid very small texts (< ngram size)
         return output
     else:
+        # 2% performance improvement; see notes.txt
+        local_separator = SEPARATOR
+        local_gram_size = GRAM_SIZE
         for i in range(len(split_text)):
             # moves a three-word (by default) "window" across the text
             current_phrase = split_text[i:i + local_gram_size]
@@ -67,6 +69,7 @@ if __name__ == "__main__":
 
     timing_data = str(datetime.now() - startTime)
     with open('performance.txt', 'a') as f:
+        # TODO: ideally would convert to milliseconds for easier parsing later
         f.write(timing_data + '\n')
         # caveat from reading SO: datetime.now() may have limited resolution,
         # be off by several ms so this isn't perfect
